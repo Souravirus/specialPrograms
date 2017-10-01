c9599593c40a580b8f4b980016088aa9112a08b4
Akshay Shrimali <akshaytryingtocode@gmail.com>
2017-10-01 15:07:05 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -2,44 +2,44 @@
  
 using namespace std;
  
-// Sort an array of size n using Bubble Sort. 
+// Function to sort an array of size n using Bubble Sort Algorithm.
 void BubbleSort (int arr[], int n)
 {
-	int i, j;
-	for (i = 0; i < n; ++i)
+    int i, k;
+    for (i = 0; i < n; ++i)
+    {
+        for (k = 0; k < n-i-1; ++k)
 	{
-		for (j = 0; j < n-i-1; ++j)
-		{
-			// Comparing values and swapping them if value at j > j+1.
-			if (arr[j] > arr[j+1])
-			{
-				arr[j] = arr[j]+arr[j+1];
-				arr[j+1] = arr[j]-arr[j + 1];
-				arr[j] = arr[j]-arr[j + 1];
-			}
-		}
-	}	
+            // Comparing values and swapping them if value at k > k+1.
+	    if (arr[k] > arr[k+1])
+	    {
+                arr[k] = arr[k]+arr[k+1];
+		arr[k+1] = arr[k]-arr[k + 1];
+		arr[k] = arr[k]-arr[k + 1];
+            }
+	}
+    }	
 }	
  
 int main()
 {
-	int n, i;
-	cout<<"\nEnter the number of data element to be sorted: ";
-	cin>>n;
+    int n, j;
+    cout<<"\nEnter the number of data element to be sorted: "; 
+    cin>>n; // Takes input of size of the array to be sorted.
  
-	int arr[n];
-	for(i = 0; i < n; i++)
-	{
-		cout<<"Enter element "<<i+1<<": ";
-		cin>>arr[i];
-	}
+    int arr[n];
+    for(j = 0; j < n; j++)
+    {
+        cout<<"Enter element "<<j+1<<": "; //Takes input of array element.
+	cin>>arr[j];
+    }
  
-	BubbleSort(arr, n);
+    BubbleSort(arr, n); // Calls BubbleSort function.
  
-	// Display the sorted data.
-	cout<<"\nSorted Data ";
-	for (i = 0; i < n; i++)
-        cout<<"->"<<arr[i];
+    // Displays the sorted array.
+    cout<<"\nSorted Data ";
+    for (j = 0; j < n; j++)
+    cout<<"-->"<<arr[j];    // Outputs the final sorted array.
  
-	return 0;
+    return 0;
 }
